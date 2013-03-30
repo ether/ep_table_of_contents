f6f58d9a5fae447981c5cb2f14f230b383f7b433
John McLear <john@mclear.co.uk>
2013-03-30 03:07:53 +0000

    improve performance

--- static/js/aceEditEvent.js
+++ static/js/aceEditEvent.js
@@ -1,3 +1,4 @@
 exports.aceEditEvent = function (hook_name, args, cb) {
+  if(!args.callstack.docTextChanged) return false; // we should only run this if the pad contents is changed..
   tableOfContents.update();
 }
