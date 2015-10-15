822a71528d9f6314d1ed53c2edceb113f9549aa4
John McLear <john@mclear.co.uk>
2015-10-15 15:59:24 +0100

    update in real time..

--- static/js/aceEditEvent.js
+++ static/js/aceEditEvent.js
@@ -1,5 +1,8 @@
 exports.aceEditEvent = function (hook_name, args, cb) {
+  if(!args.callstack.docTextChanged) return;
+  if(args.callstack.type === "setup") return;
   if(args.rep){
+    tableOfContents.update(args.rep);
     tableOfContents.showPosition(args.rep);
   }
   if(!args.callstack.docTextChanged) return false; // we should only run this if the pad contents is changed..
