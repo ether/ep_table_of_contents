e81835cc79d85732b485b62283f0562633530595
John McLear <john@mclear.co.uk>
2021-03-03 08:37:54 +0000

    fixes

--- static/js/aceEditEvent.js
+++ static/js/aceEditEvent.js
@@ -1,7 +1,7 @@
 /* global tableOfContents */
 'use strict';
 
-exports.aceEditEvent = (hookName, args, cb) => {
+exports.aceEditEvent = (hookName, args) => {
   // dont do anything on idle work timer, wait for changes..
   if (args.callstack && args.callstack.type === 'idleWorkTimer') return false;
   if (args.rep) {
